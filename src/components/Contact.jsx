b9592868c2d089472239d5f84a5be281385e480d
Emtiaz Ahmad <emtiazahmad765@gmail.com>
2024-07-13 13:01:25 +0600

    my todo

---
+++ src/components/Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <>
+   <div className=' w-full h-screen flex justify-center items-center'>
+   <div className='text-center '>
+    <h1 className='text-red-500 text-3xl font-mono'>This page is not Available</h1>
+    </div>
+   </div>
+    </>
+  )
+}
+
+export default Contact
